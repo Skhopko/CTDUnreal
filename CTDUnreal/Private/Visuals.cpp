44860cb85345ee754236fc06e5c9de903a6c24e1
Skhopko <skhopko@comcast.net>
2016-02-15 15:02:22 -0600

    Summary
    Description

---
+++ CTDUnreal/Private/Visuals.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CTDUnreal.h"
+#include "Visuals.h"
+
+
+// Sets default values
+AVisuals::AVisuals(){
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	FVector positionOne = FVector(0.0f, 0.0f, 0.0f);
+	FActorSpawnParameters params;
+	FRotator rotation = FRotator();
+	AVisuals*SphereOne = GetWorld()->SpawnActor<AVisuals>(AVisuals::StaticClass(), positionOne, rotation, params);
+
+	FVector position = PlaceNode();
+	AVisuals*SpawnSphere = GetWorld()->SpawnActor<AVisuals>(AVisuals::StaticClass(), position, rotation, params);
+}
+
+// Called when game starts or when spawned
+void AVisuals::BeginPlay(){
+	Super::BeginPlay();
+
+//	AActor*SpawnComponent(FVector const&Location);
+}
+
+// Called every frame
+void AVisuals::Tick( float DeltaTime ){
+	Super::Tick( DeltaTime );
+}
+
+//Spawns a link between two nodes
+void Link(FVector start, FVector end, float radius) {
+	FVector difference = start - end;
+
+}
+
+//Finds location of nodes
+FVector PlaceNode(){
+	FVector vec = FVector(0.0f, 0.0f, 0.0f);
+	return  vec;
+}
---
+++ CTDUnreal/Public/Visuals.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "GameFramework/Pawn.h"
+#include "Visuals.generated.h"
+
+UCLASS()
+class CTDUNREAL_API AVisuals : public AActor
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this actor's properties
+	AVisuals();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+};
